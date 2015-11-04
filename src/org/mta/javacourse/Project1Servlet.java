d91395db2b4171d315ba0813079d01de24b8f083
nadia1993 <nadiamed1993@gmail.com>
2015-11-04 17:02:09 +0200

    mutual commit

---
+++ src/org/mta/javacourse/Project1Servlet.java
@@ -0,0 +1,12 @@
+package org.mta.javacourse;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Project1Servlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
