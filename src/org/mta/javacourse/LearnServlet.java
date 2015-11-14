43f9aa50d17d8041c80e3c954ccedbbffb7ffb47
nadia1993 <nadiamed1993@gmail.com>
2015-11-14 16:31:22 +0200

    lol

--- src/org/mta/javacourse/LearnServlet.java
+++ src/org/mta/javacourse/LearnServlet.java
@@ -2,7 +2,7 @@ package org.mta.javacourse;
 
 import java.io.IOException;
 import javax.servlet.http.*;
-// we can do import to the class And it is super
+// we can do import to the class And it is hot
 
 @SuppressWarnings("serial")
 public class LearnServlet extends HttpServlet {
