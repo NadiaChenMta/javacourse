dd89c3120f61db04fdb3bcf481dd24d167657f70
nadia1993 <nadiamed1993@gmail.com>
2015-11-14 18:09:09 +0200

    check3

--- src/org/mta/javacourse/Project1Servlet.java
+++ src/org/mta/javacourse/Project1Servlet.java
@@ -7,6 +7,6 @@ import javax.servlet.http.*;
 public class Project1Servlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("chen, world");
+		resp.getWriter().println("nadia, world");
 	}
 }
