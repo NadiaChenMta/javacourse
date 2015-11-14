b66d304e83812fd648ead3eb1abac074764f8614
nadia1993 <nadiamed1993@gmail.com>
2015-11-14 20:20:36 +0200

    FIRST

---
+++ src/org/mta/javacourse/CalculationManager.java
@@ -0,0 +1,13 @@
+package org.mta.javacourse;
+import java.lang.Math;
+
+
+public class CalculationManager {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	}
+
+}
