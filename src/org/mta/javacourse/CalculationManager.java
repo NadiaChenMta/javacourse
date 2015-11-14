40a0ce4b0ece58f066c8318b3b0c7c14aa428096
Chenmu10 <Chenmu10@gmail.com>
2015-11-14 20:48:46 +0200

    task 3

--- src/org/mta/javacourse/CalculationManager.java
+++ src/org/mta/javacourse/CalculationManager.java
@@ -1,13 +1,36 @@
 package org.mta.javacourse;
-import java.lang.Math;
-
 
 public class CalculationManager {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private static double RADIUS = 50;
+	private static double HYPOTENUSE = 50;
+	private static double ANGLE = 30;
+	private static double BASE = 20;
+	private static double EXP = 13;
+	
+	private double area;
+	private double opposite;
+	private double powerRes;
 		
+	public double getArea(double radius) {
+		area = Math.PI * Math.pow(radius, 2);
+		return area;
+	}
+	
+	public double getOpposite(double hypotenuse, double angle) {
+		opposite = Math.sin(angle * Math.PI / 180) * hypotenuse;
+		return opposite;
+	}
+	
+	public double getPower(double base, double exp) {
+		powerRes = Math.pow(base, exp);
+		return powerRes;
 	}
 
+	public String getResults() {
+		String line1 = new String("1. The area of a circle with radius " + RADIUS + " is " + getArea(RADIUS));
+		String line2 = new String("2. The lenght of opposite where angle B is " + ANGLE + " and hypotenuse is " + HYPOTENUSE + " is " + getOpposite(HYPOTENUSE, ANGLE));
+		String line3 = new String("1. Power of " + BASE + " with exponent of " + EXP + " is " + getPower(BASE, EXP));
+		
+		return line1 + "<br>" + line2 + "<br>" + line3;
+	}	
 }
---
+++ src/org/mta/javacourse/Exercise3.java
@@ -0,0 +1,18 @@
+package org.mta.javacourse;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+import java.lang.Object;
+import java.lang.String;
+
+@SuppressWarnings("serial")
+public class Exercise3 extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/html");
+				
+		CalculationManager calculator = new CalculationManager();
+		String resultStr = calculator.getResults();
+		resp.getWriter().println(resultStr);
+	}
+}
