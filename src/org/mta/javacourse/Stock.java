a96a5d74c016a26d05fa44fe7f22f0b2a74519bb
nadia1993 <nadiamed1993@gmail.com>
2015-11-23 21:09:05 +0200

    ENJOY

--- src/org/mta/javacourse/Stock.java
+++ src/org/mta/javacourse/Stock.java
@@ -1,26 +1,27 @@
 package org.mta.javacourse;
-
-import java.text.ParseException;
+import java.util.Date;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+
 
 public class Stock {
 	private String symbol;
 	private Float ask;
 	private Float bid;
-	private java.util.Date date;
-		//java.util.Date date=new java.util.Date();//
+	private Date date;
+		
 	public Stock()
 	{
-		java.util.Date date=new java.util.Date();
-		symbol="unknown";
-		ask=(float) -1;
-		bid=(float) -1;		
+		date=new java.util.Date();
+		symbol="unknown";	
+		ask=null;
+		bid=null;
 	}
-	public void setAll(String symbol, Float ask, Float bid)
+	public void setAll(String symbol, Float ask, Float bid, Date date)
 	{
-		setSymbol(this.symbol);
-		setAsk(this.ask);
-		setBid(this.bid);
+		setSymbol(symbol);
+		setAsk(ask);
+		setBid(bid);
 		setDate(date);
 	}
 	public String getSymbol() {
@@ -41,31 +42,24 @@ public class Stock {
 	public void setBid(Float bid) {
 		this.bid = bid;
 	}
-	public java.util.Date getDate() {
-		return date;
+	public String getDate() {
+		DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+		String sDate;
+		sDate=df.format(date);
+		return sDate;
 	}
 	public void setDate(java.util.Date date) {
-		SimpleDateFormat dateformat2 = new SimpleDateFormat("dd-MM-yyyy");
-
-        String strdate2 = "11-15-2014";
-        try {
-
-            date = dateformat2.parse(strdate2);
-
-            System.out.println(date);
-
-        } catch (ParseException e) {
-
-            e.printStackTrace();
-
-        }
-
-	}
+		this.date=date;
+     }
+	
 	
 	public String getHtmlDescription() {
-		String resString= "<b>Stock symbol</b>: "+getSymbol()+"<b>Ask</b>: " + getAsk()+"<b>Bid</b>: " + getBid()+ "<b>Date</b>: " + getDate();
+		String resString= "<b>Stock symbol</b>: "+getSymbol()+"<b>Ask</b>: " + getAsk()+"<b>Bid</b>: "
+	+ getBid()+ "<b>Date</b>: " + getDate()+"<br>";
 		return resString;
+	}
+
 	}
 	
 	
-}
+
--- src/org/mta/javacourse/StockDetailsServlet.java
+++ src/org/mta/javacourse/StockDetailsServlet.java
@@ -1,25 +1,40 @@
 package org.mta.javacourse;
-
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.io.IOException;
+
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class StockDetailsServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/html");
-		resp.getWriter().println("hello, world");
 		
+		Date date = null;
+
+	    String expectedPattern = "MM/dd/yyyy";
+	    SimpleDateFormat formatter = new SimpleDateFormat(expectedPattern);
+	    String userInput = "11/15/2014";
+	     try {
+			date= formatter.parse(userInput);
+		} catch (ParseException e) {
+			
+			e.printStackTrace();
+		}
+
 		Stock stock1=new Stock();
 		Stock stock2=new Stock();
 		Stock stock3=new Stock();
 		
-		stock1.setAll("B", (float)1,(float)2);
-		stock2.setAll("c", (float)1,(float)5);
-		stock3.setAll("d", (float)1,(float)4);
-
+		stock1.setAll("PIH",(float)13.1,(float)12.4,date);		
+		stock2.setAll("AAL", (float)5.78,(float)5.5,date);
+		stock3.setAll("CAAS", (float)32.2,(float)31.5,date);
 		
-		stock1.getHtmlDescription();
-		stock2.getHtmlDescription();
-		stock3.getHtmlDescription();
+		
+		String resString = stock1.getHtmlDescription()+stock2.getHtmlDescription()+stock3.getHtmlDescription();
+		resp.getWriter().println(resString);
+		
 	}
 }
+	
